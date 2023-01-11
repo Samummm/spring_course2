2be97af5e96bf22de0255d152d0ee11b4bf068f2
Mad Max <samumm@mail.ru>
2023-01-11 17:59:10 +0300

    IC and DI -- properties 2

---
+++ src/main/java/org/example/Test4.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test4 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext2.xml");
+
+        context.close();
+    }
+}
