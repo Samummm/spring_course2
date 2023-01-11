8a6390faa56af1398e9e18aa83e245191fea7a02
Mad Max <samumm@mail.ru>
2023-01-11 13:00:24 +0300

    IC and DI -- properties

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -2,9 +2,35 @@ package org.example;
 
 public class Person {
     private Pet pet;
-
-    public Person(Pet pet) {
+    private  String surname;
+    private int age;
+    public Person() {
+        System.out.println("Person bean is created");
+    }
+ /**   public Person(Pet pet) {
+        System.out.println("Person bean is created");
         this.pet = pet;
+    }
+*/
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set pet");
+        this.pet = pet;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
     }
 
     public void callYourPet() {
