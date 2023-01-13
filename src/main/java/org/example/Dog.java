e7e898a6c066963845c67fcd90a7105aca9bcd20
Mad Max <samumm@mail.ru>
2023-01-13 16:00:14 +0300

    IC and DI -- properties 2

--- src/main/java/org/example/Dog.java
+++ src/main/java/org/example/Dog.java
@@ -1,6 +1,15 @@
 package org.example;
 
 public class Dog implements Pet {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     public  void say(){
         System.out.println("Bow-Wow");
